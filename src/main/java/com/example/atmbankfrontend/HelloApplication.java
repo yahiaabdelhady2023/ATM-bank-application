b936dfdb60dd4fb6c50ae76c28a1e8657c7f6613
yahiaabdelhady2023 <146368638+yahiaabdelhady2023@users.noreply.github.com>
2025-11-23 23:01:01 +0200

    added navbar function() returns HBox type, fed to other functions

--- src/main/java/com/example/atmbankfrontend/HelloApplication.java
+++ src/main/java/com/example/atmbankfrontend/HelloApplication.java
@@ -1,29 +1,54 @@
 package com.example.atmbankfrontend;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.annotation.ElementType;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Scene scene = CreateInsertCardPage();
+        HBox navbar = CreateNavBar();
+
+        Scene scene = CreateInsertCardPage(navbar);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    private Scene CreateInsertCardPage(){
+    private HBox CreateNavBar(){
+        HBox hbox = new HBox();
+
+        Text bank_name_text = new Text("ATM Name");
+        Text time_text = new Text("Sun 27");
+
+        String languages[] = {"English","Arabic","French"};
+//        ComboBox combo_box = new ComboBox(FXCollections.observableArrayList(languages));
+
+        hbox.getChildren().addAll(bank_name_text,time_text);
+        hbox.setAlignment(Pos.TOP_CENTER);
+        hbox.setSpacing(10);
+        return  hbox;
+    }
+
+    private Scene CreateInsertCardPage(HBox navbar){
         System.out.println("hello");
         StackPane stackpane = new StackPane();
 
         Button insert_card_button = new Button("Insert your card!");
+        stackpane.getChildren().add(navbar);
         stackpane.getChildren().add(insert_card_button);
 
         return new Scene(stackpane);
