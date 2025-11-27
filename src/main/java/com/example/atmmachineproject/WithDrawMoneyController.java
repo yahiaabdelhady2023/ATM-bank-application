2eee8c3d5adb78dc51ae07c116617b30b76d9cec
yahiaabdelhady2023 <146368638+yahiaabdelhady2023@users.noreply.github.com>
2025-11-27 20:36:40 +0200

    added dialog for withdrawmoneycontroller

--- src/main/java/com/example/atmmachineproject/WithDrawMoneyController.java
+++ src/main/java/com/example/atmmachineproject/WithDrawMoneyController.java
@@ -3,9 +3,14 @@ package com.example.atmmachineproject;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.Optional;
+import java.util.Optional;
 
 public class WithDrawMoneyController {
     private Stage stage;
@@ -19,5 +24,20 @@ public class WithDrawMoneyController {
         System.out.println("Money to withdraw :" + money_amount);
         money_text.setText("Money to withdraw :"+ money_amount);
 
+    }
+
+    public void OnPressConfirm(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Withdraw Money Prompt");
+        alert.setContentText("Are you sure you want to withdraw "+money_text.getText()+" ?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && result.get() == ButtonType.OK){
+            System.out.println("YES!");
+        }
+        else{
+            System.out.println("NO!");
+        }
     }
 }
