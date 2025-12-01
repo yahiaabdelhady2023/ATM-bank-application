5743fe93eec3da8d8e629666478bb4249d971a6b
yahiaabdelhady2023 <146368638+yahiaabdelhady2023@users.noreply.github.com>
2025-12-01 18:48:24 +0200

    added TableView, although needs to build model for it and populate the table

--- src/main/java/com/example/atmmachineproject/HistoryController.java
+++ src/main/java/com/example/atmmachineproject/HistoryController.java
@@ -1,4 +1,16 @@
 package com.example.atmmachineproject;
 
-public class HistoryController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HistoryController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
