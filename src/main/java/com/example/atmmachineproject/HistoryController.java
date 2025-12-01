86cb4b239c4db225166efeb2c7d12b42c3b2a870
yahiaabdelhady2023 <146368638+yahiaabdelhady2023@users.noreply.github.com>
2025-12-01 18:38:10 +0200

    basic set up for history page

---
+++ src/main/java/com/example/atmmachineproject/HistoryController.java
@@ -0,0 +1,4 @@
+package com.example.atmmachineproject;
+
+public class HistoryController {
+}
--- src/main/java/com/example/atmmachineproject/MainPageController.java
+++ src/main/java/com/example/atmmachineproject/MainPageController.java
@@ -49,5 +49,13 @@ public class MainPageController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void OnPressHistory(ActionEvent event) throws  IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("history-page.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
 
